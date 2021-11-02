c8c38dfde2ab9b44fe84c8a47d2fb6d992c960ba
Timothy Karani <kartimothy@gmail.com>
2021-11-03 01:31:08 +0300

    Added instructors urls

--- backend/urls.py
+++ backend/urls.py
@@ -34,9 +34,16 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # auth
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
+
+    # apps
+    path('api/v1/instructors/', include('instructors.urls')),
+
+    # docs
     path('swagger/', schema_view.with_ui(
         'swagger', cache_timeout=0), name="schema-swagger-ui"),
     path('redoc/', schema_view.with_ui(
