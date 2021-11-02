034bfc47c5dba50e30eff0a9bb0d0a36b2c65404
Timothy Karani <kartimothy@gmail.com>
2021-11-03 02:39:03 +0300

    added course serializer

---
+++ courses/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Course
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = (
+            'id',
+            'instructor_id',
+            'name',
+            'description',
+            'created_at',
+            'updated_at',
+        )
