486a1e666d6561dcb17fdf249cbd366c2ccbafab
Timothy Karani <kartimothy@gmail.com>
2021-11-03 02:36:37 +0300

    Added courses app

--- backend/settings.py
+++ backend/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
     # local
     'seeder',
     'instructors',
+    'courses',
 ]
 
 MIDDLEWARE = [
---
+++ courses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from safedelete.models import SafeDeleteModel
+from safedelete.models import HARD_DELETE_NOCASCADE
+
+from instructors.models import Instructor
+
+
+class Course(SafeDeleteModel):
+    # Objects will be hard-deleted, or soft deleted if other objects would have been deleted too.
+    _safedelete_policy = HARD_DELETE_NOCASCADE
+
+    instructor_id = models.OneToOneField(
+        Instructor, on_delete=models.PROTECT)
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}: {}".format(self.id, self.name)
