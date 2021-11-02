64cfcdba4d0e854ac2d90fc45fe8afe88804caf6
Timothy Karani <kartimothy@gmail.com>
2021-11-03 01:49:08 +0300

    can seed users

--- seeder/management/commands/db_seed.py
+++ seeder/management/commands/db_seed.py
@@ -1,8 +1,13 @@
 from django.core.management.base import BaseCommand
+from users.seeder import seed as seed_users
 
 
 class Command(BaseCommand):
     help = 'seed database'
 
     def handle(self, *args, **options):
-        print('seeding database')
+        print('Seeding database:')
+
+        print('Users',)
+        inserted = seed_users(200)
+        print(inserted, end='\n\n')
